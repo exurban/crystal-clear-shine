9557343dd025f40d815b4860437fe94d3ebc9e0c
Brad Gibbs <bradgibbs@mac.com>
2021-03-21 22:41:45 -0500

    fix

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import Alert from "./Alert";
+import Footer from "./Footer";
+import Meta from "./Meta";
+
+type Props = {
+  preview?: boolean;
+  children: React.ReactNode;
+};
+
+const Layout = ({ preview, children }: Props) => {
+  return (
+    <>
+      <Meta />
+      <div className="min-h-screen">
+        <Alert preview={preview} />
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
