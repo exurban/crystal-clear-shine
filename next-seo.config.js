a3dca6f2dc4735febb91707e9569b7097d4435ec
Brad Gibbs <bradgibbs@mac.com>
2021-03-22 14:18:52 -0500

    seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,7 +3,15 @@ export default {
     type: "website",
     locale: "en_IE",
     url: "https://crystal-clear-shine.vercel.app/",
-    site_name: "Crystal Clear Shine"
+    site_name: "Crystal Clear Shine",
+    images: [
+      {
+        url: "https://gyreimages.s3.us-east-2.amazonaws.com/ccs-share-card.png",
+        width: 1200,
+        height: 630,
+        alt: "OG Image"
+      }
+    ]
   },
   twitter: {
     handle: "@CrystalClear",
