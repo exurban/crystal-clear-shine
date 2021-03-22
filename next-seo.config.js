0ca3c715daf0ead13ddfc7cb103224c8ed0781f7
Brad Gibbs <bradgibbs@mac.com>
2021-03-22 13:45:16 -0500

    next-seo

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://crystal-clear-shine.vercel.app/",
+    site_name: "Crystal Clear Shine"
+  },
+  twitter: {
+    handle: "@CrystalClear",
+    site: "@CrystalClear",
+    cardType: "summary_large_image"
+  }
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "../styles/index.css";
+import { DefaultSeo } from "next-seo";
+import SEO from "../../next-seo.config";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </Layout>
   );
