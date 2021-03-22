7ffa19c7ff643afca7867c29efe155d02887344f
Brad Gibbs <bradgibbs@mac.com>
2021-03-22 09:25:42 -0500

    z

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,46 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
-        <body>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/inter-var-latin.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="/static/favicons/site.webmanifest" rel="manifest" />
+          <link
+            href="/static/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/static/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/static/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <link color="#4a9885" href="/static/favicons/safari-pinned-tab.svg" rel="mask-icon" />
+          <meta content="#ffffff" name="theme-color" />
+          <meta content="#ffffff" name="msapplication-TileColor" />
+          <meta content="/static/favicons/browserconfig.xml" name="msapplication-config" />
+        </Head>
+        <body className="bg-white">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
